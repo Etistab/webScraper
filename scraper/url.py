e4f5f0d0895cbe8fd857b170a1d242ed4e426634
qcaritey <quentin.caritey@neoxia.com>
2020-06-28 15:04:02 +0200

    added relativePath method to url lib

--- scraper/url.py
+++ scraper/url.py
@@ -9,3 +9,10 @@ def getPath(url):
 def getFilename(url):
   name = getPath(url).split('/').pop()
   return name if len(name) > 0 else 'index.html'
+
+def getRelativePath(url):
+  rel = [ seg for seg in getPath(url).split('/') if len(seg) > 0 ]
+  if len(rel) == 0:
+    return ''
+  rel.pop()
+  return '/'.join(rel)
