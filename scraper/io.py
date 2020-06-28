e9c2c2c313a6b029458482d5dbb1987781a065a5
qcaritey <quentin.caritey@neoxia.com>
2020-06-28 15:05:25 +0200

    improved scraping saves in fs

--- scraper/io.py
+++ scraper/io.py
@@ -1,7 +1,17 @@
-from os.path import join
-
+from pathlib import Path
+import os.path as path
 import scraper.url as url
+import utils.logger as logger
+
+def checkDir(dir):
+  return path.isdir(dir)
 
 def save(link, document, options):
-  with open(join(options['outputDir'], url.getFilename(link)), 'w') as f:
-    f.write(document)
+  try:
+    p = path.join(options['outputDir'], url.getDomain(link), url.getRelativePath(link))
+    Path(p).mkdir(parents=True, exist_ok=True)
+    with open(path.join(p, url.getFilename(link)), 'w') as f:
+      f.write(document)
+  except:
+    logger.info(f'An error occured while saving {link}!')
+    logger.info(f'Skipping document...')
