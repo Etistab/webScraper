87ce706a745e65b28fe3be41c291df74a46b3b3a
qcaritey <quentin.caritey@neoxia.com>
2020-06-28 17:25:12 +0200

    rewrote cli args with argparse

--- config/args.py
+++ config/args.py
@@ -1,8 +1,15 @@
-import sys
+import argparse
 import config.loader as loader
 
 def get():
   target = loader.load()
-  if len(sys.argv) > 1:
-    target['url'] = sys.argv[1]
+  target.update({ (k, v) for k, v in vars(parseArgs()).items() if v != None })
   return target
+
+def parseArgs():
+  parser = argparse.ArgumentParser(description='Scraps a website.')
+  parser.add_argument('url', metavar='URL', type=str, help='url of the website')
+  parser.add_argument('-o', '--output', type=str, dest='outputDir', help='defines the output directory')
+  parser.add_argument('-d', '--depth', type=int, dest='depth', help='defines the scraping depth')
+  parser.add_argument('-c', dest='crossorigin', action='store_const', const=True, help='allows the scraper to fetch documents from a different origin')
+  return parser.parse_args()
